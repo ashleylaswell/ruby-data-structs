b35ebbad44507b5c75043a42e9e29a298703ccfb
Ashley Laswell <lswl9898@gmail.com>
2020-11-17 14:35:51 -0500

    add example of list

--- singly_linked_list/singly_linked_list.rb
+++ singly_linked_list/singly_linked_list.rb
@@ -83,3 +83,14 @@ class Node
 		"Node with value #{@value}"
 	end
 end
+
+list = SinglyLinkedList.new
+
+list.append(10)
+list.append(20)
+list.append(30)
+
+list.append_after(10, 15)
+list.append_after(20, 25)
+
+list.print
