b47d13ad3d37388731db7cb62467a608d60ed38b
Ashley Laswell <lswl9898@gmail.com>
2020-11-17 14:33:22 -0500

    add node class to singly linked list

--- singly_linked_list/singly_linked_list.rb
+++ singly_linked_list/singly_linked_list.rb
@@ -69,3 +69,17 @@ class SinglyLinkedList
 		end
 	end
 end
+
+class Node
+	attr_accessor :next
+	attr_reader :value
+
+	def initialize(value)
+		@value = value
+		@next = nil
+	end
+
+	def to_s
+		"Node with value #{@value}"
+	end
+end
