5cbab376b4c85060c8e16c5bfa488e43489066b8
Ashley Laswell <lswl9898@gmail.com>
2020-11-20 15:54:21 -0500

    include comparable module

--- binary_search_tree/binary_search_tree.rb
+++ binary_search_tree/binary_search_tree.rb
@@ -1,6 +1,11 @@
 class Node
+	include Comparable
 	attr_accessor :left, :right
 	attr_reader :value
+
+	def <=>(other_value)
+		value <=> other_value
+	end
 
 	def initialize(value)
 		@value = value
